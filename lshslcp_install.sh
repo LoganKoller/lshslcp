6556d061896b30478b1e90f3dcde6e1829476f80
LoganKoller <59714139+LoganKoller@users.noreply.github.com>
2024-05-29 18:57:53 +0000

    Add files via upload

---
+++ lshslcp_install.sh
@@ -0,0 +1,7 @@
+curl -LJ https://api.github.com/repos/LoganKoller/lshslcp/tarball/main -o lshslcp.tar.gz
+tempdir=$(mktemp -d)
+sudo tar -xzf lshslcp.tar.gz -C "$tempdir"
+ofn=$(find "$tempdir" -mindepth 1 -maxdepth 1 -type d)
+sudo mv "$ofn" "$tempdir/lshslcp"
+sudo mv "$tempdir/lshslcp" "./"
+sudo rm -rf "$tempdir"
