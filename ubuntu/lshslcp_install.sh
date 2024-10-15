5d453cc556d6528a65d84e32a15372186db98b7f
LoganKoller <59714139+LoganKoller@users.noreply.github.com>
2024-10-15 14:01:47 -0500

    Fixes error on UNIX systems

--- ubuntu/lshslcp_install.sh
+++ ubuntu/lshslcp_install.sh
@@ -5,3 +5,5 @@ ofn=$(find "$tempdir" -mindepth 1 -maxdepth 1 -type d)
 sudo mv "$ofn" "$tempdir/lshslcp"
 sudo mv "$tempdir/lshslcp" "./"
 sudo rm -rf "$tempdir"
+sudo apt-get install dos2unix
+sudo dos2unix lshslcp/main.sh
